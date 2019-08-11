bf94de3276464bb21729e0c88896911d0da847e3
John Karrell Speck <speckworks@gmail.com>
2019-08-11 19:08:31 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -21,6 +21,8 @@ end
 
 def alan_kay_is_known_for
 	# What combination of keys would you use to return the value of the :known_for key of :alan_kay?
+	  programmer_hash[:alan_kay][:known_for]
+	
 	
 	programmer_hash = 
  		{
@@ -40,6 +42,9 @@ def alan_kay_is_known_for
 end
 
 def dennis_ritchies_language
+  programmer_hash[:dennis_ritchie][:languages]
+  
+  
 	programmer_hash = 
  		{
         :grace_hopper => {
@@ -63,6 +68,8 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
+programmer_hash[:yukihiro_matsumoto => {:known_for => "Ruby", :languages => ["LISP, "C"]}]
+
 
 	programmer_hash = 
  		{
@@ -101,7 +108,7 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
+     programmer_hash[alan_kay][:known_for => "GUI"]
      
 end
 
